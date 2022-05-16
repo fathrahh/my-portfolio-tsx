5fc441ef143c32a07d2337dc2a44c03ae004292c
fathrahh <gazoon3@gmail.com>
2022-05-16 09:30:31 +0700

    init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Navbar : FC = () => {
+    return(
+        <nav className="text-lg text-blue-400">
+            this is Header
+        </nav>
+    )
+}
+
+export default Navbar;
