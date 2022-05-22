e22be39fc349230a3746db5e3d918b487edbd0c8
fathrahh <gazoon3@gmail.com>
2022-05-22 16:23:46 +0700

    [Update] test burger logo

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import * as React from 'react'
 import { useTheme } from '../context'
 import { THEME_LIGHT } from './../const/action';
+import BurgerGrayscale from './../assets/images/burger-grayscale-removebg.png'
+import BurgerReal from './../assets/images/burger-removebg.png'
 
 export default function Main(){
   const { state } = useTheme()
+  const burgerEl = React.useRef<HTMLDivElement>(null)
+  const [ burgerPosition, setBurgerPosition ] = React.useState<number>(0)
   const { theme } = state
   const colorTheme = theme === THEME_LIGHT 
     ? "text-black"
     : "text-white"
+
+  const handleBurgerHover = (e : React.MouseEvent<HTMLElement>) : void => {
+   setBurgerPosition(e.clientY - (burgerEl.current?.getBoundingClientRect().top || e.clientY))
+  }
 
   return (
     <div className={`${colorTheme} container mx-auto flex-1 `}>
@@ -21,8 +29,19 @@ export default function Main(){
         <h1 className='text-6xl font-bold'>Hello</h1>
       </div>
       {/* Right */}
-      <div className='flex flex-col justify-center '>
-        kanan
+      <div className='flex flex-col justify-center'>
+        <div
+          className='relative overflow-hidden '
+          onMouseMove={handleBurgerHover}
+          onMouseOut={() => setBurgerPosition(0)}
+          ref={burgerEl}
+        >
+          <img src={BurgerGrayscale} className="object-cover scale-125" alt="gcburger"/>
+          <div className='absolute top-0 overflow-hidden w-full' style={{height: `${burgerPosition}px`}}>
+            <img src={BurgerReal} className="scale-125" alt="burger-real"/> 
+          </div>
+          
+        </div>
       </div>
       </section>
     </div>
